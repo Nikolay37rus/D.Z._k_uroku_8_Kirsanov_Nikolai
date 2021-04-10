bb9425f38064eb9478a2793ff0a2d98fb0028792
Nikolay37rus <nikolayhupahus@mail.ru>
2021-04-10 16:06:27 +0300

    Добавьте файлы проекта.

---
+++ BelieveOrNotBelieve/Question.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BelieveOrNotBelieve
+{
+    
+    
+    public class Question
+    {
+        string text;       
+        bool trueFalse;    
+
+        public string Text { get { return text; } set { if (value.GetType() == typeof(string)) text = value; } }
+        public bool TrueFalse { get { return trueFalse; } set { if (value.GetType() == typeof(bool)) trueFalse = value; } }
+
+        
+        
+        public Question()
+        {
+        }
+
+        
+        public Question(string text, bool trueFalse)
+        {
+            this.text = text;
+            this.trueFalse = trueFalse;
+        }
+    }
+
+}
---
+++ PutEnglishWords/Dictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PutEnglishWords
+{
+   
+    public class Dictionary : IEquatable<Dictionary>
+    {
+        string englishText;       
+        string russianText;       
+
+        public string EnglishText { get { return englishText; } set { if (value.GetType() == typeof(string)) englishText = value; } }
+        public string RussianText { get { return russianText; } set { if (value.GetType() == typeof(string)) russianText = value; } }
+
+       
+        public bool Equals(Dictionary another)
+        {
+            if (this.englishText == another.englishText && this.russianText == another.russianText)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+      
+        public Dictionary()
+        {
+        }
+
+       
+        public Dictionary(string engtext, string rutext)
+        {
+            this.englishText = engtext;
+            this.russianText = rutext;
+        }
+    }
+}
